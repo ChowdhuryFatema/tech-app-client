720725b1618eb78deba46b06a92d57e1a9d57b68
ChowdhuryFatema <fatemachowdhury318@gmail.com>
2024-06-01 14:59:40 +0600

    Navbar Added

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,49 +1,49 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 const Navbar = () => {
+
+    const navLinks = <>
+        <NavLink className="corner relative text-lg px-4 py-2 font-semibold text-white" to="/">Home</NavLink>
+        <NavLink className="text-lg px-4 py-2 font-semibold text-white" to="/products">Products</NavLink>
+    </>
+
     return (
-        <div className='max-w-7xl mx-auto px-5'>
-            <div className="navbar">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        <div className='fixed w-full top-0 left-0 z-50'>
+            <div className='max-w-7xl mx-auto px-5'>
+                <div className="navbar">
+                    <div className="navbar-start">
+                        <div className="dropdown">
+                            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            </div>
+                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                {navLinks}
+                            </ul>
                         </div>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                        <Link to="/" className="flex items-center gap-2">
+                            <img className='w-10 animate-spin' src={logo} />
+                            <span className='text-3xl font-bold text-white'>Tach Apps</span>
+                        </Link>
+                    </div>
+                    <div className="navbar-center hidden lg:flex">
+                        <ul className="menu menu-horizontal px-1">
+                            {navLinks}
                         </ul>
                     </div>
-                    <Link to="/" className="flex gap-2">
-                        <img className='w-10' src={logo} />
-                        <span className='text-2xl font-bold text-[#0ae0b8]'>Tach Apps</span>
-                    </Link>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
-                    </ul>
-                </div>
-                <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <div className="navbar-end">
+                        <div className='flex gap-4'>
+                            <Link to="/logIn" className="relative px-4 py-2 overflow-hidden group bg-[#0ae0b8] hover:bg-gradient-to-r hover:from-[#0ae0b8] hover:to-[#0ae0b8] text-black font-bold transition-all ease-out duration-300">
+                                <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+                                <span className="relative">Sign In</span>
+                            </Link>
+                            <Link to="/signUp" className="relative inline-flex items-center justify-center px-4 py-2 overflow-hidden tracking-tighter text-white font-bold bg-gray-800 group corner hover:text-black duration-500">
+                                <span className="absolute w-0 transition-all duration-500 ease-out bg-[#0ae0b8] rounded-full group-hover:w-56 h-56"></span>
+                                <span className="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
+                                <span className="relative">Sign Up</span>
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -11,24 +11,61 @@ import img5 from '../../../assets/banner.png';
 const Banner = () => {
     return (
         <div>
-            <Carousel autoPlay={true}>
+            <Carousel>
                 <div className="relative">
                     <img src={img1} className="object-center" />
-                    <div className="absolute top-1/4 left-2/4 -translate-x-[50%] bg-black bg-opacity-70 p-10">
-                        <h2 className="text-5xl text-white font-bold">Tach Web Apps</h2>
+                    <div className="absolute top-1/4 left-2/4 -translate-x-[50%] bg-black bg-opacity-65 px-10 py-14 w-full md:w-3/4 lg:w-1/2">
+                        <div className="space-y-8">
+                            <h2 className="text-3xl md:text-4xl lg:text-5xl text-white font-bold">
+                                Tach Web <span className="text-[#0ae0b8]">Apps</span>
+                            </h2>
+                            <p className="text-white opacity-80">
+                                TechConnect is a dynamic web app that connects tech enthusiasts and professionals through forums, tutorials, and collaborative opportunities.
+                            </p>
+                            <div className="pt-4">
+                                <a href="#_" className="relative px-5 py-2 font-medium text-black group">
+                                    <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-[#085f4f] group-hover:bg-[#0ae0b8] group-hover:skew-x-12"></span>
+                                    <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-[#0ae0b8] group-hover:bg-[#0e977e] group-hover:-skew-x-12"></span>
+                                    <span className="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-[#0ae0b8] -rotate-12"></span>
+                                    <span className="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-[#0ae0b8] -rotate-12"></span>
+                                    <span className="relative group-hover:text-white">Button Text</span>
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div>
+                <div className="relative">
                     <img src={img2} className="object-center" />
+                    <div className="absolute top-1/4 left-2/4 -translate-x-[50%] bg-black bg-opacity-70 p-10">
+                        <h2 className="text-3xl md:text-4xl lg:text-5xl text-white font-bold">
+                            Tach Web <span className="text-[#0ae0b8]">Apps</span>
+                        </h2>
+                    </div>
                 </div>
-                <div>
+                <div className="relative">
                     <img src={img3} className="object-center" />
+                    <div className="absolute top-1/4 left-2/4 -translate-x-[50%] bg-black bg-opacity-70 p-10">
+                        <h2 className="text-5xl text-white font-bold">
+                            Tach Web <span className="text-[#0ae0b8]">Apps</span>
+                        </h2>
+                    </div>
                 </div>
-                <div>
+                <div className="relative">
                     <img src={img4} className="object-center" />
+                    <div className="absolute top-1/4 left-2/4 -translate-x-[50%] bg-black bg-opacity-70 p-10">
+                        <h2 className="text-5xl text-white font-bold">
+                            Tach Web <span className="text-[#0ae0b8]">Apps</span>
+                        </h2>
+
+                    </div>
                 </div>
-                <div>
+                <div className="relative">
                     <img src={img5} className="object-center" />
+                    <div className="absolute top-1/4 left-2/4 -translate-x-[50%] bg-black bg-opacity-70 p-10">
+                        <h2 className="text-5xl text-white font-bold">
+                            Tach Web <span className="text-[#0ae0b8]">Apps</span>
+                        </h2>
+                    </div>
                 </div>
 
             </Carousel>
